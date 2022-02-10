07325b54017a61de123a29dc4f89dd8a3a90c166
anitavkapari <anitavkapari@gmail.com>
2022-02-10 22:51:24 +0530

    use autowired

--- src/main/java/com/spring/jdbc/JdbcConfig.java
+++ src/main/java/com/spring/jdbc/JdbcConfig.java
@@ -3,14 +3,13 @@ package com.spring.jdbc;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-import com.spring.jdbc.dao.StudentDao;
-import com.spring.jdbc.dao.StudentDaoImpl;
-
 @Configuration
+@ComponentScan(basePackages = {"com.spring.jdbc.dao"})
 public class JdbcConfig {
 	
 	@Bean("ds")
@@ -30,12 +29,12 @@ public class JdbcConfig {
 		return jdbcTemplate;
 	}
 	
-	@Bean("studentDao")
-	public StudentDao getStudentDao() {
-		StudentDaoImpl studentDao = new StudentDaoImpl();
-		studentDao.setJdbcTemplate(getTemplate());
-		return studentDao;
-	}
+//	@Bean("studentDao")
+//	public StudentDao getStudentDao() {
+//		StudentDaoImpl studentDao = new StudentDaoImpl();
+//		studentDao.setJdbcTemplate(getTemplate());
+//		return studentDao;
+//	}
 	
 	
 }
